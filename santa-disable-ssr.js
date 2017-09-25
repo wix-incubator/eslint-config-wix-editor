a843afddc1d1207b0b26ebdfc9417ed6d47e9941
Nir Naor <nirnaori@gmail.com>
2017-09-25 18:58:42 +0300

    Adding santa-disable-ssr config file.

---
+++ santa-disable-ssr.js
@@ -0,0 +1,16 @@
+'use strict';
+// santa ssr package
+module.exports = {
+    "extends": ["./santa.js"],
+    "rules": {
+        "santa/no-module-state": 0,
+        "santa/no-experiment-in-component": 0,
+        "no-restricted-syntax": [
+            "error",
+            {
+                "selector": "CallExpression[callee.type='MemberExpression'][callee.object.name='experiment'][callee.property.name='isOpen'][arguments.length<1]",
+                "message": "isOpen must always be invoked with at least one argument."
+            }
+        ],
+    }
+};
