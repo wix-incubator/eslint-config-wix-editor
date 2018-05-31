65f68db2a81102ea055c25d92086004304614bc8
idok <idok@wix.com>
2018-05-31 12:54:05 +0300

    add npm package config

---
+++ santa-package-esm.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = {
+    "extends": ["./santa-package.js"],
+    "parserOptions": {
+        "sourceType": "module"
+    }
+}
---
+++ santa-package.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "plugins": ["lodash", "react"],
+    "extends": ["./node", "plugin:lodash/recommended"],
+    "parser": "babel-eslint",
+    "rules": {
+      "sort-imports": 0,
+      "lodash/no-commit": 2,
+      "lodash/path-style": [0, "as-needed"],
+      "lodash/prefer-constant": 0,
+      "lodash/prefer-filter": 2,
+      "lodash/prefer-lodash-method": 0,
+      "lodash/prefer-lodash-typecheck": 0,
+      "lodash/prefer-map": 2,
+      "lodash/prefer-matches": 2,
+      "lodash/prefer-noop": 0,
+      "lodash/chaining": 0,
+      "lodash/import-scope": 0,
+      "lodash/prefer-startswith": 0,
+      "no-unused-vars": [2, { "varsIgnorePattern": "React$" }],
+      "react/jsx-uses-vars": 2,
+      "react/react-in-jsx-scope": 2,
+      "react/display-name": 2,
+      "react/no-danger": 2,
+      "react/no-did-mount-set-state": 2,
+      "react/no-did-update-set-state": 2,
+      "react/no-unknown-property": 2,
+      "react/prop-types": [2, {
+        "ignore": [
+          "children"
+        ]
+      }],
+      "react/no-unused-prop-types": 2,
+      "react/sort-comp": 2,
+      "react/self-closing-comp": 2,
+      "react/jsx-wrap-multilines": 2,
+      "react/jsx-key": 2,
+      "react/jsx-pascal-case": 2
+    },
+    "env": {
+      "browser": true,
+      "commonjs": true,
+      "jasmine": true
+    }
+  }
+  
